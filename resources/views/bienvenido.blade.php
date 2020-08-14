47958a15216344699edda0b803304b4d3032f877
George <kniazyouk@gmail.com>
2020-08-14 18:57:39 -0400

    primer commit

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Programacion avanzada</title>
+    <style>
+        h1{
+            color: blue;
+        }
+        .rojo{
+            color: red;
+        }
+    </style>
+</head>
+<body>
+    <h1>Programacion Avanzada</h1>
+    <p class="rojo">Este es el portal de la materia</p>
+    <p><b>Gestion:</b> 2 / 2020</p>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () { 
+    return view('welcome');
+});
+
+Route::get('hola', function(){
+    return '<h1>HOLA USFA!!!</h1>';
+});
+
+Route::get('foo', function () {
+    return 'Hello World';
+});
+
+Route::get('user/{id}', function ($id) {
+    return 'User '.$id;
+});
+
+Route::get('saludar/{nombre}', function ($nombre) {
+    //return '<h1>Hola ' . $nombre . ' como estas?</h1>';
+    return "Hola $nombre como estas?";
+});
+
+Route::get('acercade', function(){
+    return view('bienvenido');
+});
